b54ead2379ef9f7e8cb4d78f0620af44fb62c4ed
Vlad Golubev <golubev.vld@gmail.com>
2016-09-18 22:25:53 +0300

    Initial implementation

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const restify = require('restify');
 const server = restify.createServer();
 
-server.pre(restify.pre.userAgentConnection());
-server.pre(restify.pre.sanitizePath());
+const snippetsHandler = require('./lib/handlers/snippets');
+const articleHandler = require('./lib/handlers/article');
 
+server.pre(restify.pre.userAgentConnection());
+
+server.use(restify.queryParser());
 server.use(restify.throttle({
   burst: 10,
   rate: 3,
   ip: true
 }));
 
-function respond(request, response, next) {
-  response.send('hello ' + request.params.name);
-  next();
-}
-
-server.get('/hello/:name', respond);
+server.get('/snippets/:page?', snippetsHandler);
+server.get('/article', articleHandler);
 
 server.listen(9191, () => {
   console.log('%s listening at %s', server.name, server.url);
